ef2f238bcfedc75918daf77fccb7a54d739d103e
harshitsingh7848 <singhharshit7848@gmail.com>
2021-10-25 17:39:35 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
+echo "hello there";
 
-$msg = $_GET['msg'];
-$hashed_msg = hash_hmac('sha256', $msg, 'test#lsq$8909');
-
-$response = array("secret_token"=> $hashed_msg);
-echo json_encode($response);
 
 ?>
